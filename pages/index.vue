4db6fb6fcd7dab60bfec303bec8d1c4fb2939645
Burak Osman <burakosman.yaldiz@hotmail.com>
2024-06-24 22:44:23 +0300

    adressing issue fixing #2

--- pages/index.vue
+++ pages/index.vue
@@ -3,9 +3,26 @@
 <div class="flex flex-col space-y-4 pb-12">
   
   <ProfileCard />
-  <StatusCard />
+  <Statuscard />
   <Aboutme id="aboutme"/>
   <Projects id="projects"/>
 </div>
 
 </template>
+
+<script>
+import ProfileCard from '~/components/ProfileCard.vue';
+import Statuscard from '~/components/StatusCard.vue';
+import Aboutme from '~/components/AboutMe.vue';
+import Projects from '~/components/Projects.vue';
+
+export default {
+  components: {
+    ProfileCard,
+    Statuscard,
+    Aboutme,
+    Projects
+  },
+  // Your data, methods, computed properties, etc.
+}
+</script>
